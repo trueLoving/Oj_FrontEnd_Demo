393e2361af1ef9fe5bd3522c2b72c7fc38031f1a
star sky <1176789241@qq.com>
2020-05-31 22:29:39 +0800

    基本组件引入完成

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="home">
-   
+   <CodeEditor language="C"></CodeEditor>
   </div>
 </template>
 
 <script>
 
+import CodeEditor from "@/components/CodeEditor";
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components:{
+    CodeEditor
+  }
 }
 </script>
